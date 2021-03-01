9061e3831030f3011f0563c675e6c4ddc1e1f920
Mahmoud Nasr <ma7moud.asp@gmail.com>
2021-03-01 21:42:01 +0200

    Calculator Design Static Resources Window Resources Styles Application Resources

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "7";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}7";
+            }
+        }
+
+        private void eightButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "8";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}8";
+            }
+        }
+
+        private void nineButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "9";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}9";
+            }
+
+        }
+
+        private void fourButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "4";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}4";
+            }
+        }
+
+        private void fiveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "5";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}5";
+            }
+        }
+
+        private void sixButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "6";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}6";
+            }
+        }
+
+        private void threeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "3";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}3";
+            }
+        }
+
+        private void twoButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "2";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}2";
+            }
+        }
+
+        private void oneButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "1";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}1";
+            }
+        }
+
+        private void zeroButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Content.ToString())
+            {
+                case "/":
+                      resultLabel.Content = $"{ resultLabel.Content}/";
+                    break;
+                case "AC":
+                    resultLabel.Content = "0";
+                    break;
+                case "=":
+                    DataTable dt = new DataTable();
+                    var v = dt.Compute(resultLabel.Content.ToString(), "");
+                    resultLabel.Content = v.ToString();
+                    break;
+                    //default:
+
+            }
+        }
+    }
+}
